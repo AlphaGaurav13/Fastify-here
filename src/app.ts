6fd0f6d30c5442b21b122a1271a154ece8b12ef9
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2026-06-24 18:40:09 +0000

    done with sse

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import Fastify from 'fastify'
 
+import { notificationRoutes } from "./routes/notification.route.ts"
 
 const app = Fastify({
-    logger:true,
-})
-
-app.get("/", async () => {
-    return {
-        message: "Hello Fastify"
-    };
+    logger: true
 });
+
+
+
+app.register(notificationRoutes);
 
 app.listen({
     port: 3000,
-});
+})
--- src/routes/notification.route.ts
+++ src/routes/notification.route.ts
@@ -1,11 +1,36 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 
-import { addClient, removeClient, broadcast } from '../services/notification.service.js';
+import  { addClient, removeClient, broadcast } from '../services/notification.service.ts';
 
 export async function notificationRoutes(
     fastify: FastifyInstance
 ) {
     fastify.get("/events", async (request, reply) => {
+        reply.raw.writeHead(200, {
+            "Content-type": "text/event-stream",
+            "Cache-control": "no-cache",
+            Connection: "keep-alive"
+        });
 
-    })
+        reply.raw.write("data: Connected\n\n");
+
+
+        addClient(reply.raw);
+
+        request.raw.on("close", () => {
+            removeClient(reply.raw);
+        });
+    });
+
+    fastify.post<{
+        Body: {
+            message: string;
+        };
+    }>("/notify", async (request) => {
+        broadcast(request.body.message);
+
+        return {
+            success: true,
+        };
+    });
 }
